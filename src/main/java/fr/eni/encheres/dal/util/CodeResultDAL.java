aea1a07dbb7e7da4b3ddf078d55c26193978eacc
mbenoit2023 <mbenoit2023@44SE34-3RV6YW2.ad.campus-eni.fr>
2023-08-29 12:16:44 +0200

    BusinessException

---
+++ src/main/java/fr/eni/encheres/dal/util/CodeResultDAL.java
@@ -0,0 +1,7 @@
+package fr.eni.encheres.dal.util;
+
+public abstract class CodeResultDAL {
+	public static final int INSERT_OBJECT_NULL = 10_000;
+	//public static final int INSERT_AVIS_NOTE_ECHEC = 10_002;
+	public static final int INSERT_OBJECT_ECHEC = 10_001;
+}
